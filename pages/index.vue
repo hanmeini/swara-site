941280b2b0a82acc48689f6b43c3f7da5c8e6582
bokem29 <byandnadif28@gmail.com>
2025-01-04 22:39:44 +0700

    image

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="font-sans bg-gradient-to-b from-white to-red-50">
+
 
     <!-- Hero Section -->
     <section class="px-8 py-24 lg:flex lg:justify-between lg:items-center font-jakarta">
       <div class="lg:w-1/2">
-        <h1 class="text-5xl leading-tight  font-bold  text-gray-800">
-          <span class="text-red-500  italic font-semibold">Suara</span> Anda, 
+        <h1 class="text-5xl leading-tight font-bold  text-gray-800">
+          <span class="text-red-500 italic font-semibold">Suara</span> Anda, 
           <span class="italic underline decoration-red-600 decoration-4"><br/>Perubahan</span><br /> kami.
         </h1>
-        <p class="mt-6 text-gray-600">
-          Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.
-        </p>
+        
         <a
           href="#"
           class="inline-flex items-center px-6 py-3 mt-6 text-white bg-red-500 rounded-full hover:bg-red-600"
@@ -18,14 +16,11 @@
           Sampaikan keluhanmu <span class="ml-2">➔</span>
         </a>
       </div>
-      <div class="grid grid-cols-2 gap-4 mt-12 lg:mt-0 lg:w-1/2">
+      <div class="grid grid-cols-2 gap-3 mt-16 lg:mt-0 lg:w-1/2">
         <!-- Card 1 -->
-        <div class="p-4 bg-green-100 rounded-lg shadow">
-          <img src="https://via.placeholder.com/150" alt="Recycle" class="rounded-md" />
-          <h3 class="mt-4 text-lg font-bold">Lorem ipsum dolor sit amet</h3>
-          <a href="#" class="flex items-center mt-2 text-red-500 hover:text-red-700">
-            Telusuri <span class="ml-1">➔</span>
-          </a>
+        <div class="rounded-3xl relative overflow-hidden shadow group max-w-60">
+          <img src="./../assets/image/card1.png" alt="Recycle" class="transition-transform group-hover:scale-105 duration-300" />
+      
         </div>
         <!-- Card 2 -->
         <div class="p-4 bg-red-100 rounded-lg shadow">
@@ -47,7 +42,7 @@
         </div>
       </div>
     </section>
-  </div>
+  
 </template>
 
 
